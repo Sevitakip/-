c3955989501c473169f86a585af61dbbe56feee8
Sevitakip <148937441+Sevitakip@users.noreply.github.com>
2023-11-14 18:34:21 +0300

    Add files via upload

---
+++ _ya-tr-span data-index=_1-0_ data-translated=_true_ data-source-lang=_en_ data-target-lang=_ru_ data-value=_main.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def EuclrdrCO(a,b):
+    print(a,b)
+    while a/=0 and b/=0
+    if a>b:
+        a=a%b
+        else
+        b=b%a
+        print(a+b)
